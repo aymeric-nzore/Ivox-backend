8afbbf005e2cd43e774b919e9d311987dcc956de
aymeric-nzore <aymeric.nzore2024@esatic.edu.ci>
2026-03-29 10:25:59 +0000

    auth => admin

--- routes/levelRoutes.js
+++ routes/levelRoutes.js
@@ -1,11 +1,12 @@
 import express from "express";
 import { saveLevelWords, addXpAndScore, addCoins } from "../controllers/levelController.js";
 import authMiddleware from "../middlewares/authMiddleware.js";
+import adminMiddleware from "../middlewares/adminMiddleware.js";
 
 const router = express.Router();
 
 // Générer/enregistrer les mots d'un niveau
-router.post("/save-words", authMiddleware, saveLevelWords);
+router.post("/save-words", adminMiddleware, saveLevelWords);
 // Ajouter XP et score à la fin d'un niveau
 router.post("/add-xp-score", authMiddleware, addXpAndScore);
 // Ajouter des pièces
